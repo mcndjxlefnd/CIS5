0ed1fd5dcf921b0013ddc61ffa25a9d0a71f4cf0
Lyle <LyleLengyel@gmail.com>
2020-09-17 10:43:08 -0700

    complete 3-10.py

--- crash/3-10.py
+++ crash/3-10.py
@@ -1,7 +1,5 @@
 generic_list = ['first', 'second', 'third', 'fourth', 'fifth', 'sixth', 'seventh', 'eighth', 'ninth', 'tenth']
 
-fourth_item_message = ' is the fourth item in the list, indexed at three:'
-
 print(f"{generic_list[3].title()} is the fourth item in the list, indexed at three.\n")
 
 print(f"{generic_list[-1].title()} is the last item in the list, indexed at 9.\n")
@@ -12,29 +10,36 @@ print(f"{generic_list[0].title()} is the first item in the list, indexed at zero
 
 generic_list.append('eleventh')
 
-print(generic_list)
+print("\nAppended 'eleventh': ", generic_list)
 
 generic_list.insert(2, '1.5')
 
-print(f"{generic_list[2]} is the third item in the list, indexed at two. {generic_list[3]} has moved to index 3.")
+print(f"\n{generic_list[2]} should be '1.5' and is the third item in the list, indexed at two. {generic_list[3]} has moved to index 3.")
 
 del generic_list[2]
-print(generic_list)
+print("\n 1.5 has been deleted from the list: ", generic_list)
 
 popped_glist = generic_list.pop()
 
-print("Popped:", popped_glist, "\ngeneric_list result:", generic_list)
+print("\nPopped:", popped_glist, "\ngeneric_list result:", generic_list)
 
-print(f"Pop out that {generic_list.pop(1)}. Resultng list: {generic_list}")
+print(f"\nPop out that {generic_list.pop(1)}. Resultng list: {generic_list}")
 
 to_be_removed = 'fifth'
 
 generic_list.remove(to_be_removed)
 
-print("generic_list with fifth removed", generic_list)
+print("\ngeneric_list with fifth removed", generic_list)
 
+print("\nalphabetically sorted list:", sorted(generic_list))
 
+print("\noriginal list: ", generic_list)
 
+generic_list.reverse()
+print("\noriginal list reversed", generic_list)
 
+generic_list.sort()
+print("\nalphabetically sorted, permanently: ", generic_list)
 
-
+generic_list.sort(reverse=1)
+print("\nreverse alphabetically sorted in reverse: ", generic_list)
